6bcc1f6bcc6ab3b33cfc227e78e8a0a62fcf4713
Kazi Ahmed <kaaziahmed@gmail.com>
2022-03-11 15:42:21 -0500

    added favicon

---
+++ client/components/Board.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Board() {
+    return (
+        <div className="board">
+            <div>
+                <h3> To Do </h3>
+            </div>
+            <div>
+                <h3> In Porgres  </h3>
+            </div>
+            <div>
+                <h3> Completed </h3>
+            </div>
+        </div>
+    )
+}
+
+export default Board;
